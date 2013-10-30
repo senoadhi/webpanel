50f5820bb82458505a15c44bead3fca501159fe9
Seno Adhi Nugroho <simone.void@gmail.com>
2013-10-30 03:06:20 -0700

    commit function for model user

--- application/models/model_user.php
+++ application/models/model_user.php
@@ -1,8 +1,42 @@
-<?php
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
  * Created by JetBrains PhpStorm.
  * User: senonugroho
  * Date: 10/30/13
  * Time: 4:50 PM
- * To change this template use File | Settings | File Templates.
+ * Desc : Model class dipakai untuk data bersangkutan dengan user
  */
+
+class Model_user extends CI_Model
+{
+
+    function __construct()
+    {
+        parent::__construct();
+    }
+
+    #check username n password admin playground
+    function check_login($user_name,$password)
+    {
+        // $count  = $this->db->query("SELECT userid, username, password from register WHERE username = '".$user_name."' AND password ='".$password."' LIMIT 1" )->result_array();
+
+        $this->db->select('id, username, password');
+        $this->db->from('users');
+        $this->db->where('username', $user_name);
+        $this->db->where('password', $password);
+        $this->db->limit(1);
+
+        $query = $this->db->get();
+
+        if($query->num_rows() == 1)
+        {
+            return $query->result_array();
+        }
+        else
+        {
+            return FALSE;
+        }
+    }
+
+}
+?>
