35ba21ca9d7108d1cd15aa2880fd39ac1a93d4ff
Seno Adhi Nugroho <simone.void@gmail.com>
2013-10-30 03:08:59 -0700

    commit home n login views

--- application/views/home.php
+++ application/views/home.php
@@ -1,8 +1,1 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: senonugroho
- * Date: 10/30/13
- * Time: 4:55 PM
- * To change this template use File | Settings | File Templates.
- */
+<p>home page</p>
--- application/views/login.php
+++ application/views/login.php
@@ -1,8 +1,61 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: senonugroho
- * Date: 10/30/13
- * Time: 4:55 PM
- * To change this template use File | Settings | File Templates.
- */
+<div id="login-holder">
+    <div id="logo-login">
+    </div>
+    <div class="clear"></div>
+
+    <form action="/login" method="POST">
+
+        <div id="loginbox">
+            <div id="login-inner">
+                <table border="0" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <th>Username</th>
+                        <td>
+                            <input class="login-inp" type="text" name="username" value="<?php echo $_COOKIE['remember_me']?>"/>
+                            <?php echo form_error('username','<div style="color: red;">', '</div>');?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Password</th>
+                        <td>
+                            <input type="password" onfocus="this.value=''" class="login-inp" name="password"/>
+                            <?php echo form_error('password','<div style="color: red;">', '</div>');?>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td valign="top">
+                            <input type="checkbox" class="checkbox-size" id="login-check" value="1" name="remember"/>
+                            <label for="login-check">Remember me</label>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td><input type="submit" class="submit-login"  /></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="clear"></div>
+            <a href="" class="forgot-pwd">Forgot Password?</a>
+        </div>
+    </form>
+    <form>
+        <div id="forgotbox">
+            <div id="forgotbox-text">Please send us your email and we'll reset your password.</div>
+            <div id="forgot-inner">
+                <table border="0" cellpadding="0" cellspacing="0">
+                    <tr>
+                        <th>Email address:</th>
+                        <td><input type="text" value=""   class="login-inp" /></td>
+                    </tr>
+                    <tr>
+                        <th> </th>
+                        <td><input type="button" class="submit-login"  /></td>
+                    </tr>
+                </table>
+            </div>
+            <div class="clear"></div>
+            <a href="" class="back-login">Back to login</a>
+        </div>
+    </form>
+</div>
