d16a4f09bc469db752caa680bb5c65bfaeae6f1b
Seno Adhi Nugroho <simone.void@gmail.com>
2013-10-30 02:42:59 -0700

    test commit message

---
+++ application/views/test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: senonugroho
+ * Date: 10/30/13
+ * Time: 4:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "ini hanya test..";
+?>
