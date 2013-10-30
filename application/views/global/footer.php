ddbfe9fcd3ccd9c54c2c215aebc6ac5dd5da2ed0
Seno Adhi Nugroho <simone.void@gmail.com>
2013-10-30 03:10:48 -0700

    commit global views

--- application/views/global/footer.php
+++ application/views/global/footer.php
@@ -1,8 +1,7 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: senonugroho
- * Date: 10/30/13
- * Time: 4:57 PM
- * To change this template use File | Settings | File Templates.
- */
+<div id="footer">
+    <div id="footer-left">
+        Mockinup Admin Panel &copy; Copyright Seno Adhi Nugroho. <a href="">www.mockinup.com</a>. All rights reserved.</div>
+    <div class="clear">&nbsp;</div>
+</div>
+</body>
+</html>
--- application/views/global/login_header.php
+++ application/views/global/login_header.php
@@ -1,8 +1,29 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: senonugroho
- * Date: 10/30/13
- * Time: 4:58 PM
- * To change this template use File | Settings | File Templates.
- */
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+    <meta name="description" content="" />
+
+    <meta name="keywords" content="" />
+
+    <meta name="author" content="" />
+
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Mockinup - Admin Panel</title>
+    <link rel="stylesheet" href="/themes/css/screen.css" type="text/css" media="screen" title="default" />
+    <script src="/themes/js/jquery/jquery-1.4.1.min.js" type="text/javascript"></script>
+    <script src="/themes/js/jquery/custom_jquery.js" type="text/javascript"></script>
+    <script src="/themes/js/jquery/jquery.pngFix.pack.js" type="text/javascript"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $(document).pngFix( );
+        });
+    </script>
+</head>
+<body id="login-bg">
