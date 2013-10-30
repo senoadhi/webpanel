905b51671ced8ac04fbc163c0710ceb0966a5e78
Seno Adhi Nugroho <simone.void@gmail.com>
2013-10-30 03:02:24 -0700

    add function to controller home n login

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -1,8 +1,50 @@
-<?php
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * Created by JetBrains PhpStorm.
+ * Created by Seno Adhi Nugroho.
  * User: senonugroho
  * Date: 10/30/13
  * Time: 4:48 PM
- * To change this template use File | Settings | File Templates.
  */
+
+class Home extends CI_Controller
+{
+
+    function __construct()
+    {
+
+        parent::__construct();
+        $this->_user = $this->session->all_userdata();
+
+        $this->load->model('model_user');
+    }
+    function index()
+    {
+
+        $sess_data = $this->session->userdata('logged_in');
+
+        if($sess_data == FALSE)
+        {
+            redirect('login');
+        }
+
+        $this->load->view(GLOBAL_TEMPLATE.'/header');
+        $this->load->view('home');
+        $this->load->view(GLOBAL_TEMPLATE.'/footer');
+
+    }
+
+    function features()
+    {
+        $this->load->view(GLOBAL_TEMPLATE.'/header');
+        $this->load->view('features');
+        $this->load->view(GLOBAL_TEMPLATE.'/footer');
+    }
+
+    function logout()
+    {
+        $this->session->unset_userdata('logged_in');
+        session_destroy();
+        redirect('login');
+    }
+
+}
--- application/controllers/login.php
+++ application/controllers/login.php
@@ -1,4 +1,4 @@
-<?php
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
 /**
  * Created by JetBrains PhpStorm.
  * User: senonugroho
@@ -6,3 +6,94 @@
  * Time: 4:49 PM
  * To change this template use File | Settings | File Templates.
  */
+
+class Login extends CI_Controller
+{
+
+    function __construct()
+    {
+
+        parent::__construct();
+        $this->_user = $this->session->all_userdata();
+
+        $this->load->model('model_user');
+    }
+
+    function index()
+    {
+
+        if($this->session->userdata('logged_in') == TRUE)
+        {
+            redirect('home');
+        }
+
+        #Build post data
+        $username = $this->input->post('username');
+        $password = md5($this->input->post('password'));
+        $rememberme = $this->input->post('remember');
+
+        #set validation rules
+        $this->form_validation->set_rules('username', 'Username', 'required');
+        $this->form_validation->set_rules('password', 'Password', 'required');
+        $this->form_validation->set_message('required','field tidak boleh kosong..');
+
+        if ($this->form_validation->run() == FALSE)
+        {
+
+            $this->load->view(GLOBAL_TEMPLATE.'/login_header');
+            $this->load->view('login');
+            $this->load->view(GLOBAL_TEMPLATE.'/login_footer');
+            return;
+        }
+        else
+        {
+            $data_user = $this->model_user->check_login($username, $password);
+
+            if ($data_user)
+            {
+                $newdata = array('logged_in' => array(
+                    'id' => $data_user[0]['id'],
+                    'username' => $username
+                )
+                );
+                $this->session->set_userdata($newdata);
+
+                #Adding Remember me functionality
+                $year = time() + 31536000;
+
+                if($rememberme)
+                {
+                    setcookie('remember_me', $username, $year);
+                }
+                elseif(!$rememberme)
+                {
+                    if(isset($_COOKIE['remember_me']))
+                    {
+                        $past = time() - 100;
+                        setcookie('remember_me','gone', $past);
+                    }
+                }
+
+                redirect('home');
+            }
+            else
+            {
+                redirect('login');
+            }
+        }
+
+
+    }
+
+    function logout()
+    {
+        $this->session->sess_destroy('logged_in');
+
+        $this->load->view(GLOBAL_TEMPLATE.'/login_header');
+        $this->load->view('login');
+        $this->load->view(GLOBAL_TEMPLATE.'/login_footer');
+    }
+
+}
+
+?>
